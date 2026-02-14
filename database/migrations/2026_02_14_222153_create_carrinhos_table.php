b35a9c5f5125389a3dccbccfb81e5bdd9a7a6f45
Breno <brenomachado.3064@gmail.com>
2026-02-14 19:27:25 -0300

    chore: remove tabela redundante e sincroniza banco de dados

--- database/migrations/2026_02_14_222153_create_carrinhos_table.php
+++ database/migrations/2026_02_14_222153_create_carrinhos_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('carrinho', function (Blueprint $table) {
+        Schema::create('carrinhos', function (Blueprint $table) {
             $table->id();
             $table->integer('quantidade');
 
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('carrinho');
+        Schema::dropIfExists('carrinhos');
     }
 };
