7498be97d4e310baac7bd936cc0d20d53f8168b1
Breno <brenomachado.3064@gmail.com>
2026-02-22 15:47:02 -0300

    feat: implementa factory de admins

---
+++ database/factories/AdminFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+            'is_admin' => true,
+            'birthdate' => fake()->date(),
+            'cpf' => fake()->numerify('###.###.###-##'),
+            'saldo' => 0.00,
+        ];
+    }
+}
